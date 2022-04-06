688507b9eb776fbec562dc1c01977ef4db2ed62b
Stan <stantonmoore42@gmail.com>
2022-04-05 23:45:39 -0400

    change art -> cover

--- models/records.js
+++ models/records.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const recordSchema = new mongoose.Schema({
     artist: {type: String, required: true},
     title: {type: String, required: true},
-    art: {type: String, required: true},
+    cover: {type: String, required: true},    //img link
     year: {type: Number, required: true},
     genre: {type: String, required: true}
 });
